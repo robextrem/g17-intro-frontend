d88b68850467d84c4504350a621402002389dbe4
Víctor M. Reyes <victor.reyes@focusservices.com>
2022-09-19 18:53:33 -0600

    chore: create home component

---
+++ react-with-vite/src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <div className="App">
+      <div>
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+export { Home };
