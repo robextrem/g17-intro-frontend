f81a80f5e1efb3689226bcd0a978dd6e798dda6f
Roberto Romero <roberto@ulama.co>
2022-09-27 19:45:28 -0500

    feat: implementacion de axios y react

---
+++ axios/src/components/List/List.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const List = () => {
+
+  const [pokemones, setPokemones] = useState([]);
+
+  
+  useEffect(()=> {
+    axios.get(`https://raw.githubusercontent.com/oicrruf/g15-computer-science/develop/ejercicios/pokedex-registro/json/pokemon.json`)
+      .then(response => {
+        //La llamada es correcta
+        const pokeData = response.data;
+        //pokemones = pokeData; ESTO ESTA MAL HECHO
+        setPokemones(pokeData);
+        //console.log(pokemones);
+      })
+      .catch(error => {
+        //Hubo un errrrrror
+        console.log(error);
+      })
+  },[]);
+
+  return (
+    <ul className="App">
+      {
+        pokemones.map(pokemon =>{
+          return (<li key={pokemon.number}><img src={pokemon.ThumbnailImage}/> {pokemon.name}</li>)
+        })
+      }
+    </ul>
+  )
+}
+
+export {List};
